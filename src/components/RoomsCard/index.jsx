45e18554005bfbd1a2c3be8f0a0bc24f864bd4cf
zahrasabila <zahra.sabila22@mhs.uinjkt.ac.id>
2025-01-09 21:43:40 +0700

    fix: concflicts assets

--- src/components/RoomsCard/index.jsx
+++ src/components/RoomsCard/index.jsx
@@ -2,27 +2,26 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-export default function RoomsCard({ classname, imageCard, id, tipe }) {
+export default function RoomsCard({ classname, imageCard, id, tipe, desc, logoFacility, logoFacility1, logoFacility2, textFacility, textFacility1, textFacility2}) {
   const router = useRouter();
 
   return (
     <div className={`flex flex-row gap-24 ${classname}`}>
-      <div className="w-1/2 h-[30rem] bg-gray-300">
-        <Image src={imageCard} width={500} height={500} />
+      <div className="w-1/2 h-[30rem]">
+        <Image src={imageCard} width={600} height={500} />
       </div>
-      <div className="space-y-10">
+      <div className="space-y-12">
         <h1 className="text-4xl">{tipe}</h1>
-        <p className="max-w-lg text-lg">
-          Rasakan kenyamanan dan kemewahan di Kamar Deluxe 2 Bed kami. Dirancang
-          untuk memberikan pengalaman menginap yang tak terlupakan, kamar ini...
-        </p>
-        <div className="flex flex-row gap-4">
-          <p>facility</p>
-          <p>facility</p>
-          <p>facility</p>
+        <p className="max-w-lg text-lg">{desc}</p>
+        <div className="flex flex-nowrap gap-4">
+          <Image src={logoFacility} width={60} height={60} /> <p className="">{textFacility}</p>
+          <Image src={logoFacility1} width={50} height={42} /> <p className="">{textFacility1}</p>
+          <Image src={logoFacility2} width={50} height={50} /> <p className="">{textFacility2}</p>
         </div>
       </div>
-      <button onClick={() => router.push(`/rooms/${id}`)}>click</button>
+      <div className="flex flex-col place-content-center pt-36 space-y-28" >
+      <button className= "mx-auto p-4 font-semibold border-[#AE9578] border-2 w-fit text-[#AE9578]" onClick={() => router.push(`/rooms/${id}`)}>click</button>
+      </div>
     </div>
   );
 }
