6f2f11fd2ff8f4c7fb12686bbbff8704c63578d5
zahrasabila <zahra.sabila22@mhs.uinjkt.ac.id>
2025-01-20 15:15:14 +0700

    feat: booking layout

--- src/pages/booking/[slug]/index.jsx
+++ src/pages/booking/[slug]/index.jsx
@@ -1,13 +1,20 @@
 import Image from "next/image";
 import { useParams } from "next/navigation";
-import React from "react";
+import React, { use } from "react";
 import Form from "next/form";
 import { rooms } from "@/lib/constants";
+import { useRouter } from "next/router";
+import BookingLayout from "@/components/Layouts";
 
 export default function BookingPage() {
   const params = useParams();
+  const router = useRouter();
   const slug = params?.slug;
 
+  const ButtonSubmit = (e) => {
+    e.preventDefault();
+    router.push(`/payment`);
+  };
   // const rooms = [
   //   {
   //     id: 1,
@@ -65,162 +72,164 @@ export default function BookingPage() {
   //   },
   // ];
   return (
-    <section className="min-h-screen sm:py-24 flex flex-col sm:p-40">
-      <div className="flex flex-col m-auto px-24 items-center  py-7 mt-9 bg-[#F9F9F9] gap-10">
-        <div className="space-y-7">
-          <h1 className="uppercase font-poppins font-semibold text-center">
-            room type
-          </h1>
-          <div className="border-2 border-gold p-4 flex flex-row gap-6">
-            <Image
-              src={rooms[slug - 1]?.imageCard}
-              width={6000}
-              height={4000}
-              alt={rooms[slug - 1]?.tipe}
-              className="w-40 aspect-[9/6] object-cover"
-            />
-            <div className="flex flex-col justify-between">
-              <h3 className="font-libre_baskerville">
-                {rooms[slug - 1]?.tipe}
-              </h3>
-              <div className="flex flex-row flex-wrap gap-3">
-                {rooms[slug - 1]?.logoFacility.map((item, index) => (
-                  <div className="flex flex-row items-center gap-2" key={index}>
-                    <Image
-                      src={item}
-                      width={20}
-                      height={20}
-                      alt={rooms[slug - 1]?.textFacility[index]}
-                    />
-                    <p className="font-poppins text-sm">
-                      {rooms[slug - 1]?.textFacility[index]}
-                    </p>
-                  </div>
-                ))}
+      <BookingLayout>
+          <div className="space-y-7">
+            <h1 className="uppercase font-poppins font-semibold text-center">
+              room type
+            </h1>
+            <div className="border-2 border-gold p-4 flex flex-row gap-6">
+              <Image
+                src={rooms[slug - 1]?.imageCard}
+                width={6000}
+                height={4000}
+                alt={rooms[slug - 1]?.tipe}
+                className="w-40 aspect-[9/6] object-cover"
+              />
+              <div className="flex flex-col justify-between">
+                <h3 className="font-libre_baskerville">
+                  {rooms[slug - 1]?.tipe}
+                </h3>
+                <div className="flex flex-row flex-wrap gap-3">
+                  {rooms[slug - 1]?.logoFacility.map((item, index) => (
+                    <div
+                      className="flex flex-row items-center gap-2"
+                      key={index}
+                    >
+                      <Image
+                        src={item}
+                        width={20}
+                        height={20}
+                        alt={rooms[slug - 1]?.textFacility[index]}
+                      />
+                      <p className="font-poppins text-sm">
+                        {rooms[slug - 1]?.textFacility[index]}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+                <p className="font-poppins font-semibold">
+                  IDR {rooms[slug - 1]?.harga}{" "}
+                  <span className="text-xs font-normal">/night</span>
+                </p>
               </div>
-              <p className="font-poppins font-semibold">
-                IDR {rooms[slug - 1]?.harga}{" "}
-                <span className="text-xs font-normal">/night</span>
-              </p>
             </div>
           </div>
-        </div>
-        <Form className="flex flex-col gap-y-4">
-          <h2 className="font-poppins font-semibold uppercase">
-            enter your details
-          </h2>
-          <div className="grid grid-cols-2 grid-rows-3 gap-x-16 gap-y-4">
-            <div className="space-y-1 row-start-1">
-              <label
-                htmlFor="name"
-                className="font-poppins text-sm font-semibold"
-              >
-                Nama
-                <span className="font-poppins font-semibold text-red-500">
-                  *
-                </span>
-              </label>
-              <input
-                type="text"
-                id="name"
-                className="w-full p-1 border-b-2 font-poppins text-xs px-0 border-gold bg-transparent outline-none"
-                placeholder="Enter Name"
-              />
+          <Form className="flex flex-col gap-y-4">
+            <h2 className="font-poppins font-semibold uppercase">
+              enter your details
+            </h2>
+            <div className="grid grid-cols-2 grid-rows-3 gap-x-16 gap-y-4">
+              <div className="space-y-1 row-start-1">
+                <label
+                  htmlFor="name"
+                  className="font-poppins text-sm font-semibold"
+                >
+                  Nama
+                  <span className="font-poppins font-semibold text-red-500">
+                    *
+                  </span>
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  className="w-full p-1 border-b-2 font-poppins text-xs px-0 border-gold bg-transparent outline-none"
+                  placeholder="Enter Name"
+                />
+              </div>
+              <div className="space-y-1 row-start-2">
+                <label
+                  htmlFor="ktp"
+                  className="font-poppins text-sm font-semibold"
+                >
+                  Identity Number (KTP)
+                  <span className="font-poppins font-semibold text-red-500">
+                    *
+                  </span>
+                </label>
+                <input
+                  type="number"
+                  id="ktp"
+                  className="w-full p-1 border-b-2 font-poppins text-xs px-0 border-gold bg-transparent outline-none"
+                  placeholder="Enter Identity Number (KTP)"
+                />
+              </div>
+              <div className="space-y-1 row-start-3">
+                <label
+                  htmlFor="no-hp"
+                  className="font-poppins text-sm font-semibold"
+                >
+                  Phone Number
+                  <span className="font-poppins font-semibold text-red-500">
+                    *
+                  </span>
+                </label>
+                <input
+                  type="number"
+                  id="no-hp"
+                  className="w-full p-1 border-b-2 font-poppins text-xs px-0 border-gold bg-transparent outline-none"
+                  placeholder="Enter Phone Number"
+                />
+              </div>
+              <div className="space-y-1 row-start-1">
+                <label
+                  htmlFor="check-in"
+                  className="font-poppins text-sm font-semibold"
+                >
+                  Check-In Date
+                  <span className="font-poppins font-semibold text-red-500">
+                    *
+                  </span>
+                </label>
+                <input
+                  type="date"
+                  id="check-in"
+                  className="w-full p-1 border-b-2 font-poppins text-xs px-0 border-gold bg-transparent outline-none"
+                  placeholder="Enter Check-Out Date"
+                />
+              </div>
+              <div className="space-y-1 row-start-2">
+                <label
+                  htmlFor="check-out"
+                  className="font-poppins text-sm font-semibold"
+                >
+                  Check-Out Date
+                  <span className="font-poppins font-semibold text-red-500">
+                    *
+                  </span>
+                </label>
+                <input
+                  type="date"
+                  id="check-out"
+                  className="w-full p-1 border-b-2 font-poppins text-xs px-0 border-gold bg-transparent outline-none"
+                  placeholder="Enter Check-Out Date"
+                />
+              </div>
+              <div className="space-y-1 row-start-3">
+                <label
+                  htmlFor="email"
+                  className="font-poppins text-sm font-semibold"
+                >
+                  Email
+                  <span className="font-poppins font-semibold text-red-500">
+                    *
+                  </span>
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="w-full p-1 border-b-2 font-poppins text-xs px-0 border-gold bg-transparent outline-none"
+                  placeholder="Enter Email"
+                />
+              </div>
             </div>
-            <div className="space-y-1 row-start-2">
-              <label
-                htmlFor="ktp"
-                className="font-poppins text-sm font-semibold"
-              >
-                Identity Number (KTP)
-                <span className="font-poppins font-semibold text-red-500">
-                  *
-                </span>
-              </label>
-              <input
-                type="number"
-                id="ktp"
-                className="w-full p-1 border-b-2 font-poppins text-xs px-0 border-gold bg-transparent outline-none"
-                placeholder="Enter Identity Number (KTP)"
-              />
-            </div>
-            <div className="space-y-1 row-start-3">
-              <label
-                htmlFor="no-hp"
-                className="font-poppins text-sm font-semibold"
-              >
-                Phone Number
-                <span className="font-poppins font-semibold text-red-500">
-                  *
-                </span>
-              </label>
-              <input
-                type="number"
-                id="no-hp"
-                className="w-full p-1 border-b-2 font-poppins text-xs px-0 border-gold bg-transparent outline-none"
-                placeholder="Enter Phone Number"
-              />
-            </div>
-            <div className="space-y-1 row-start-1">
-              <label
-                htmlFor="check-in"
-                className="font-poppins text-sm font-semibold"
-              >
-                Check-In Date
-                <span className="font-poppins font-semibold text-red-500">
-                  *
-                </span>
-              </label>
-              <input
-                type="date"
-                id="check-in"
-                className="w-full p-1 border-b-2 font-poppins text-xs px-0 border-gold bg-transparent outline-none"
-                placeholder="Enter Check-Out Date"
-              />
-            </div>
-            <div className="space-y-1 row-start-2">
-              <label
-                htmlFor="check-out"
-                className="font-poppins text-sm font-semibold"
-              >
-                Check-Out Date
-                <span className="font-poppins font-semibold text-red-500">
-                  *
-                </span>
-              </label>
-              <input
-                type="date"
-                id="check-out"
-                className="w-full p-1 border-b-2 font-poppins text-xs px-0 border-gold bg-transparent outline-none"
-                placeholder="Enter Check-Out Date"
-              />
-            </div>
-            <div className="space-y-1 row-start-3">
-              <label
-                htmlFor="email"
-                className="font-poppins text-sm font-semibold"
-              >
-                Email
-                <span className="font-poppins font-semibold text-red-500">
-                  *
-                </span>
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="w-full p-1 border-b-2 font-poppins text-xs px-0 border-gold bg-transparent outline-none"
-                placeholder="Enter Email"
-              />
-            </div>
-          </div>
-          <button
-            type="submit"
-            className="font-poppins hover:bg-transparent hover:border-[1px] hover:border-gold hover:text-gold text-white px-4 py-2 bg-gold uppercase w-fit text-sm mx-auto my-2"
-          >
-            booking
-          </button>
-        </Form>
-      </div>
-    </section>
+            <button
+              type="submit"
+              className="font-poppins hover:bg-transparent hover:border-[1px] hover:border-gold hover:text-gold text-white px-4 py-2 bg-gold uppercase w-fit text-sm mx-auto my-2"
+              onClick={ButtonSubmit}
+            >
+              booking
+            </button>
+          </Form>
+      </BookingLayout>
   );
 }
---
+++ src/pages/payment/index.jsx
@@ -0,0 +1,10 @@
+import BookingLayout from "@/components/Layouts";
+import React from "react";
+
+export default function PaymentPage() {
+  return (
+    <div>
+      <BookingLayout>payment</BookingLayout>
+    </div>
+  );
+}
